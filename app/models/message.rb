71ef0a0137254863740b837c3b0a3db27cad5a25
hosokawa3 <clover091039@gmail.com>
2024-04-09 12:10:55 +0000

    [Update]notification_DM

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,20 @@
 class Message < ApplicationRecord
   belongs_to :end_user
   belongs_to :room
+  has_one :notification, as: :notifiable, dependent: :destroy
+  
+  #通知
+  after_create do
+    create_notification(end_user_id: self.room.entries.where.not(end_user_id: self.end_user_id).first.end_user_id)
+  end
+
+  def notification_message
+    "#{end_user.name}さんからメッセージが届いています"
+  end
+
+  def notification_path
+    "/rooms/#{room.id}"
+  end
 
   validates :message, presence: true, length: { maximum: 140 }
 end
